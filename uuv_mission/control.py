45dcb932b8f16b1154eb68b0cce074a7dbf62d3d
ElliotMcLeod26 <elliot.mcleod@bnc.ox.ac.uk>
2024-10-20 13:10:13 +0100

    Cleaned up code

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -1,37 +1,19 @@
 # control.py
 
-# class Controller:
-#     def __init__(self, Kp=0.15, Kd=0.6):
-#         self.Kp = Kp
-#         self.Kd = Kd
-#         self.previous_error = 0
-
-#     def compute_control_action(self, current_error):
-#         # Compute the derivative of the error
-#         derivative_error = current_error - self.previous_error
-        
-#         # Compute the control action
-#         control_action = self.Kp * current_error + self.Kd * derivative_error
-        
-#         # Update the previous error
-#         self.previous_error = current_error
-        
-#         return control_action
-    
 class Controller:
     def __init__(self, Kp: float, Kd: float):
         # Proportional and Derivative gains for the controller
-        self.Kp = Kp
-        self.Kd = Kd
-        self.previous_error = 0  # For storing the error at the previous timestep
+        self.Kp_gain = Kp
+        self.Kd_gain = Kd
+        self.prev_error = 0  # Store the previous timestep error
 
-    def control(self, reference: float, observation: float) -> float:
-        # Calculate error
-        error = reference - observation
-        # PD control formula: u[t] = Kp * e[t] + Kd * (e[t] - e[t-1])
-        derivative = error - self.previous_error
-        control_action = self.Kp * error + self.Kd * derivative
-        # Store the current error as previous error for the next timestep
-        self.previous_error = error
-        return control_action
-    
+    def control(self, setpoint: float, feedback: float) -> float:
+        # Calculate current error
+        current_error = setpoint - feedback
+        # Compute the derivative (change in error)
+        error_delta = current_error - self.prev_error
+        # PD control action: u[t] = Kp * e[t] + Kd * Δe[t]
+        control_output = self.Kp_gain * current_error + self.Kd_gain * error_delta
+        # Update the previous error for the next timestep
+        self.prev_error = current_error
+        return control_output
